3841074a8750a2c2ee27cc3ebe6b354a09172a9e
MiaoHN <582418227@qq.com>
2021-12-29 22:21:43 +0800

    精简策略模式代码

--- 策略模式/main.cpp
+++ 策略模式/main.cpp
@@ -5,111 +5,64 @@ public:
   Operation() {}
   ~Operation() {}
 
-  double m_NumA;
-  double m_NumB;
-
-  virtual double Calculate() = 0;
+  virtual double Calculate(double numA, double numB) = 0;
 };
 
 class OperationAdd : public Operation {
 public:
-  double Calculate() override {
-    double result = m_NumA + m_NumB;
+  double Calculate(double numA, double numB) override {
+    double result = numA + numB;
     return result;
   }
 };
 
 class OperationSub : public Operation {
 public:
-  double Calculate() override {
-    double result = m_NumA - m_NumB;
+  double Calculate(double numA, double numB) override {
+    double result = numA - numB;
     return result;
   }
 };
 
 class OperationMul : public Operation {
 public:
-  double Calculate() override {
-    double result = m_NumA * m_NumB;
+  double Calculate(double numA, double numB) override {
+    double result = numA * numB;
     return result;
   }
 };
 
 class OperationDiv : public Operation {
 public:
-  double Calculate() override {
-    if (m_NumB == 0) {
+  double Calculate(double numA, double numB) override {
+    if (numB == 0) {
       std::cout << "除数不能为0" << std::endl;
       return 0;
     }
-    double result = m_NumA / m_NumB;
+    double result = numA / numB;
     return result;
   }
 };
 
 class OperationContext {
 public:
-  OperationContext(char operation) {
-    OperationAdd *cs0 = new OperationAdd();
-    OperationSub *cs1 = new OperationSub();
-    OperationMul *cs2 = new OperationMul();
-    OperationDiv *cs3 = new OperationDiv();
-    switch (operation) {
-    case '+':
-      cs = cs0;
-      break;
-    case '-':
-      cs = cs1;
-      break;
-    case '*':
-      cs = cs2;
-      break;
-    case '/':
-      cs = cs3;
-      break;
-    }
-  }
+  OperationContext(Operation *oper) { cs = oper; }
+
   ~OperationContext() {}
 
-  void SetA(double numA) {
-    if (cs == nullptr) {
-      return;
-    }
-    cs->m_NumA = numA;
+  double GetResult(double numA, double numB) {
+    return cs->Calculate(numA, numB);
   }
-
-  void SetB(double numB) {
-    if (cs == nullptr) {
-      return;
-    }
-    cs->m_NumB = numB;
-  }
-
-  double GetA() {
-    if (cs == nullptr) {
-      return -1;
-    }
-    return cs->m_NumA;
-  }
-
-  double GetB() {
-    if (cs == nullptr) {
-      return -1;
-    }
-    return cs->m_NumB;
-  }
-
-  double GetResult() { return cs->Calculate(); }
 
 private:
   Operation *cs;
 };
 
 int main() {
-  OperationContext oper('+');
-  oper.SetA(1);
-  oper.SetB(2);
-  std::cout << oper.GetA() << " + " << oper.GetB() << " = " << oper.GetResult()
+  OperationContext oper(new OperationAdd());
+  double numA = 1;
+  double numB = 2;
+  std::cout << numA << " + " << numB << " = " << oper.GetResult(numA, numB)
             << std::endl;
   return 0;
 }
